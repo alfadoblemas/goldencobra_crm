a3d47c026f4d999bd832825be74ae7ef8f71e224
Holger Frohloff <holger.frohloff@gmail.com>
2012-06-07 17:59:43 +0200

    [30851509] customer started 	modified:   admin/customers.rb 	new file:   config/locales/activerecord.de.yml 	modified:   test/dummy/config/initializers/active_admin.rb 	modified:   test/dummy/config/routes.rb 	modified:   test/dummy/log/test.log

--- admin/customers.rb
+++ admin/customers.rb
@@ -1,4 +1,4 @@
-ActiveAdmin.register GoldencobraCrm::Customer do
-  #menu parent: "CRM", if: proc{can?(:read, GoldencobraCrm::Customer)}
-  #controller.authorize_resource class: GoldencobraCrm::Customer
+ActiveAdmin.register GoldencobraCrm::Customer, as: "Customer" do
+  menu parent: "CRM", if: proc{can?(:read, GoldencobraCrm::Customer)}
+  controller.authorize_resource class: GoldencobraCrm::Customer
 end
